83fdca959ecb94656fe789e13ff5c3c9355fb829
Denzel Wamburu <denzjoseph@gmail.com>
2015-12-11 19:06:54 +0300

    populated with dummy data models

--- www/js/videos/videos.ctrl.js
+++ www/js/videos/videos.ctrl.js
@@ -1,3 +1,14 @@
 app.controller('VideosCtrl', function($scope, $http){
-  $scope.videos = [];
+  $scope.videos = [
+    {
+  title: "My first video",
+  date: "1-1-2015",
+  thumbnails: "http://i.ytimg.com/vi/bJp1ptX4F3M/maxresdefault.jpg",
+},
+{
+  title: "My second video",
+  date: "5-7-2015",
+  thumbnails: "http://i.ytimg.com/vi/NA2VerbOyt0/maxresdefault.jpg",
+}
+  ];
 });
